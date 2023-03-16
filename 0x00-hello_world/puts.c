be66d44834fa6f567e5eae39a643d901f2a5c985
Zemichael <yetikimitabeba@gmail.com>
2023-03-16 20:33:15 +0000

    Task 4

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * program that prints string using the puts() function
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main()
 {
 	char myStr[] = "Programming is like building a multilingual puzzle";
