cfcbad5d5feb1c4e379d4a2cd031fbcab147c51e
Zemichael <yetikimitabeba@gmail.com>
2023-03-17 16:39:31 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%i", a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
