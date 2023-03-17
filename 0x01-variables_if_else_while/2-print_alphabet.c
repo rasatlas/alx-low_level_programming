fa9454f81d0e47e7e919a33575100daf51978463
Zemichael <yetikimitabeba@gmail.com>
2023-03-17 16:31:44 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+
+	return (0);
+}
