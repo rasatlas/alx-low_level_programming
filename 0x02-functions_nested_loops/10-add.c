6f00a2c351b0e7f0c7096efd6621a4596316ceaf
root <root@Suntilaye>
2023-03-22 02:25:49 +0300

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - is a function that adds two integers and returns the result.
+ *
+ * @num1: is the first integer values provided by a user.
+ * @num2: is the second integer value provided by a user.
+ *
+ * Return: is the summation of the two passed numbers.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
