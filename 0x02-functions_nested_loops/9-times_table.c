59e1d51915a324b12e4b1462bb23e10818116cb2
root <root@Suntilaye>
2023-03-22 01:40:20 +0300

    Task 8-8-24_hours

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printNumber - prints a number if it is more than two digits
+ * and less than 3 digits.
+ *
+ * times_table - is a function that prints the 9 times table, starting with 0.
+ *
+ * @num: is a product of row and column and is more than 9.
+ *
+ */
+
+void _printNumber(int num)
+{
+	int i, j;
+
+	i = num / 10;
+	j = num % 10;
+	_putchar(i + '0');
+	_putchar(j + '0');
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * times_table - is a function that prints the 9 times table, starting with 0.
+ *
+ */
+
+void times_table(void)
+{
+	int row, column, p;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			p = (row * column);
+			if (p > 9)
+			{
+				_printNumber(p);
+			}
+			else
+			{
+				_putchar(p + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
