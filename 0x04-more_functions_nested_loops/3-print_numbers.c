1ebf4d9493331ea60e78b94b8cb75307e033358f
= <=>
2023-03-24 00:05:43 +0300

    Task 3 The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - is a function that prints the numbers,
+ * print_numbers - is a function that prints the numbers,
  * from 0 to 9, followed by a new line.
  */
 
