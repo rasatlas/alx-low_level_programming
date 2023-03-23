e5faf34f1d4eccc3ad5399f23b14fc4a95f324d8
root <root@SUN>
2023-03-23 21:15:13 +0300

    Task 2 betty identation fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if ((a > b) && (a > c))
-		{
-			largest = a;
-		}
+	{
+		largest = a;
+	}
 	else if ((a > b) && (c > a))
-		{
-			largest = c;
-		}
+	{
+		largest = c;
+	}
 	else if (b > c)
-		{
-			largest = b;
-		}
+	{
+		largest = b;
+	}
 	else
 	{
 		largest = c;
