e8235e5f6e69180e32d7df2eaae0bb8bebd1e0a7
= <=>
2023-03-27 21:13:15 +0300

    4. I can only go one way. I ve not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
+ * _strlen - a function that returns the length of a string.
+ *
+ * @s: char pointer
+ *
+ * Return: length of the string, 0 is null
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * print_rev - is a function that prints a string, in reverse,
  * followed by a new line.
  *
  * @s: char pointer
  */
+
 
 void print_rev(char *s)
 {
