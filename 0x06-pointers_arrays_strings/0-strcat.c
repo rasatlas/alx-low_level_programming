85e39e9bac135e216a9ce0baa5d963c7559b8b66
= <=>
2023-03-30 16:10:13 +0300

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - is a function that concatenates two strings.
+ *
+ * @dest: is a char pointer to a string
+ * @src: is a char pointer to a string
+ *
+ * Return: pointer to dest string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	return (dest);
+}
