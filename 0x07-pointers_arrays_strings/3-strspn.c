e62e0cfe150d0baac4e861eeb1a4372b9e67739c
= <=>
2023-04-03 23:07:48 +0300

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,4 +12,38 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int a, b;
+	char *d;
+
+	d = s;
+	while (*accept != '\0')
+	{
+		s = d;
+		while ((*s != ' ') || (*s != ','))
+		{
+			if (*s == *accept)
+			{
+				break;
+			}
+			else
+				continue;
+			s++;
+		}
+		if (*s != *accept)
+		{
+			a  = 0;
+			break;
+		}
+		accept++;
+
+	}
+	if (*accept == '\0')
+	{
+		b = 0;
+		s = d;
+		while ((*s != ' ') || (*s != ','))
+			b++;
+		a = b;
+	}
+	return (a);
 }
