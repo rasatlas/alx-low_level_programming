a8430d472a674e1c39662699826de0a63bcc539f
= <=>
2023-04-03 22:54:37 +0300

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strspn - is a function that gets the length of a prefix substring.
+ *
+ * @s: string to be scanned.
+ * @accept: string containing the characters to match.
+ *
+ * Return: the number of characters in the intitial segment of s
+ * which consists only of characters from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
