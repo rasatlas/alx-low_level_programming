daab12efef774f560abeb7a852b020c6303c483f
Zemichael <yetikimitabeba@gmail.com>
2023-04-04 14:46:14 -0700

    Edit for Task 5 and 9

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,31 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
+
+/*
+
+char *_strstr(char *haystack, char *needle)
+{
 	char *ptrH = haystack;
 	char *ptrN = needle;
 	char *ptr;
@@ -39,3 +64,4 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return ('\0');
 }
+*/
