29e830378c5a7703c765ab7d1a823a48d35d1a08
Zemichael <yetikimitabeba@gmail.com>
2023-04-04 15:50:31 -0700

    main.h prototypes edit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
