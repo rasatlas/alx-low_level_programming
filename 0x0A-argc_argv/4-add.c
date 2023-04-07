a4ccf59f246ecad9618f71df82f32f697cc48309
= <=>
2023-04-08 00:01:27 +0300

    Task 4 - a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,32 +7,27 @@
  * Return: if no number is passed to the program, print 0, otherwise 1.
  */
 
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
+	char *ptr;
+	int num;
 
 	if (argc > 1)
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 1; argv[i]; i++)
 		{
-			if (isNumber(argv[i]))
-			{
-				sum += (atoi(argv[i]));
-			}
+			num = strtol(argv[i], &p, 10);
+			if (!*ptr)
+				sum += num;
 			else
 			{
 				printf("%s\n", "Error");
 				return (1);
 			}
 		}
-		printf("%i\n", sum);
-		return (0);
 	}
-	else
-	{
-		printf("%i\n", 0);
-		return (1);
-	}
-
+	printf("%i\n", sum);
+	return (0);
 }
