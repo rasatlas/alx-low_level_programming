6acda8e383a675c6551e3e137d814a3485c5acd6
= <=>
2023-04-08 00:26:50 +0300

    Task 5 - a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,63 @@
 #include "main.h"
 
 /**
+ * _changer - changes notes into cents.
+ * @note: value to changed into different denominations of cents.
+ * Return: note interms of cents.
+ */
+
+int _changer(int note)
+{
+	if (note == 0)
+	{
+		return (0);
+	}
+	else if (note >= 25)
+	{
+		return (1 + _changer(note - 25));
+	}
+	else if (note >= 10)
+	{
+		return (1 + _changer(note - 10));
+	}
+	else if (note >= 5)
+	{
+		return (1 + _changer(note - 5));
+	}
+	else if (note >= 2)
+	{
+		return (1 + _changer(note - 2));
+	}
+	else
+	{
+		return (1 + _changer(note - 1));
+	}
+}
+
+/**
  * main - entry point and a program that prints the minimum number
  * of coins to make change for an amount of money.
- *
  * @argc: argument count.
  * @argv: argument vector.
- *
  * Return: 1 if the number of arguments passed is not exactly 1,
  * 0 if the number passed as the argument is negative.
  */
 
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int coins = 0;
-	int money;
+	int note;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		if (strchr(argv[argc - 1], '-'))
-		{
-			printf("0\n");
-			return (1);
-		}
-
-		money = atoi(argv[argc - 1]);
-
-		while (money > 0)
-		{
-			if (money % 25 == 0)
-			{
-				money -= 25;
-			} else if (money % 10 == 0)
-			{
-				money -= 10;
-			} else if (money % 5 == 0)
-			{
-				money -= 5;
-			} else if (money % 2 == 0)
-			{
-				money -= 2;
-			} else
-			{
-				money--;
-			}
-			coins++;
-		}
-		printf("%d\n", coins);
+		printf("%s\n", "Error");
+		return (1);
+	}
+	note = atoi(argv[1]);
+	if (note < 0)
+	{
+		printf("%i\n", 0);
 		return (0);
 	}
-	printf("Error\n");
-	return (1);
+	printf("%i\n", _changer(note));
+	return (0);
 }
