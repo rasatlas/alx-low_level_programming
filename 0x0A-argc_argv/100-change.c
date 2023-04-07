ebbe87b4dc4669fbd72e28711e7f7c449a335326
= <=>
2023-04-07 18:45:43 +0300

    Task 5 - a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,39 +16,39 @@ int main(int argc, char *argv[])
 	int coins = 0;
 	int money;
 
-	if (argc == 1 || argc > 2)
+	if (argc == 2)
 	{
-		printf("%s\n", "Error");
-		return (1);
-	}
-	else if (strchr(argv[argc - 1], '-'))
-	{
-		printf("%i\n", 0);
+		if (strchr(argv[argc - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+
+		money = atoi(argv[argc - 1]);
+
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+			} else if (money % 10 == 0)
+			{
+				money -= 10;
+			} else if (money % 5 == 0)
+			{
+				money -= 5;
+			} else if (money % 2 == 0)
+			{
+				money -= 2;
+			} else
+			{
+				money--;
+			}
+			coins++;
+		}
+		printf("%i\n", coins);
 		return (0);
 	}
-
-	money = atoi(argv[argc - 1]);
-
-	while (money > 0)
-	{
-		if (money % 25 == 0)
-		{
-			money -= 25;
-		} else if (money % 10 == 0)
-		{
-			money -= 10;
-		} else if (money % 5 == 0)
-		{
-			money -= 5;
-		} else if (money % 2 == 0)
-		{
-			money -= 2;
-		} else
-		{
-			money--;
-		}
-		coins++;
-	}
-	printf("%i\n", coins);
-	return (0);
+	printf("%s\n", "Error");
+	return (1);
 }
