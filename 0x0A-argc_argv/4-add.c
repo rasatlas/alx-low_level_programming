5082aad673ed7f55687b14a8707646bb56124fff
= <=>
2023-04-07 23:35:11 +0300

    Task 4 - a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * isNumber -  checks if a number is digit or not.
+ *
+ * @arg: pointer to a number to be checked for being digit.
+ *
+ * Return: 1 if digit, otherwise 0.
+ */
+
+int isNumber(char *arg)
+{
+	int i = 0;
+
+	if (arg[0] == '-')
+		i = 1;
+
+	for (; arg[i] != '\0'; i++)
+	{
+		if (!isdigit(arg[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
  * main - entry point & a program that adds positive numbers.
  *
  * @argc: argument count.
@@ -11,25 +36,32 @@
  * followed by a new line, and return 1.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
+	int i;
 	int sum = 0;
-	char *ptr;
 
-	for (int i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		ptr = argv[i];
-		for (int j = 0; ptr[j] != '\0'; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(ptr[j]))
+			if (isNumber(argv[i]))
+			{
+				sum += (atoi(argv[i]));
+			}
+			else
 			{
 				printf("%s\n", "Error");
 				return (1);
 			}
 		}
-		sum += atoi(ptr);
+		printf("%i\n", sum);
+		return (0);
 	}
-	printf("%i\n", sum);
-	return (0);
-}
+	else
+	{
+		printf("%i\n", 0);
+		return (1);
+	}
 
+}
