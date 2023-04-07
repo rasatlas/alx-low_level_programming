b8d3df59aacb7368695fdc55225afcf98e1baedc
= <=>
2023-04-07 13:51:53 +0300

    Task 3 - a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point and a program that multiplies two numbers.
+ *
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 1 always.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		printf("%s\n", "Error");
+	else
+		printf("%d\n", ((atoi(argv[1])) * (atoi(argv[2]))));
+	return (1);
+}
