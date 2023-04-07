e4945ee1c5511fde4dcd84136657de38c4f2ffff
= <=>
2023-04-07 13:54:40 +0300

    Task 3 - a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,6 +15,6 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 		printf("%s\n", "Error");
 	else
-		printf("%d\n", ((atoi(argv[1])) * (atoi(argv[2]))));
+		printf("%i\n", ((atoi(argv[1])) * (atoi(argv[2]))));
 	return (1);
 }
