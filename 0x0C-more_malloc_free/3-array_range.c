e8127ee62faea3a2b3503de80ec7e888361f60fd
= <=>
2023-04-12 22:36:39 +0300

    Task 3 - a function that creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,9 +11,12 @@
 
 int *array_range(int min, int max)
 {
+	int *ptr;
+
 	if (min > max)
 		return (NULL);
-	int *ptr = malloc(sizeof(int) * ((max - min) + 1));
+
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
 
 	if (ptr == NULL)
 		return (NULL);
