79ad358f40baf86e3ad4e51b0e1be7ceb53c382b
= <=>
2023-04-12 23:18:38 +0300

    Task 4 - a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,20 +1,13 @@
 #include "main.h"
-
 /**
  * _realloc - a function that reallocates a memory block using malloc and free
- *
- * @ptr: a pointer to the memory previously allocated with a call to malloc:
- * malloc(old_size)
- *
+ * @ptr: a pointer to the memory previously allocated with a call to malloc
  * @old_size: the size, in bytes, of the allocated space for ptr
- *
  * @new_size: the new size, in bytes of the new memory block
- *
  * Return: If new_size == old_size return ptr, if new_size is equal to zero,
  * and ptr is not NULL, then the call is equivalent to free(ptr) - return NULL
  * otherwise, a pointer to the reallocated memory block.
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newPtr;
@@ -23,40 +16,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-
 	if (ptr == NULL)
 	{
 		newPtr = malloc(new_size);
-
 		if (newPtr == NULL)
 			return (NULL);
-
 		return (newPtr);
 	}
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	ptrCopy = ptr;
 	newPtr = malloc(sizeof(*ptrCopy) * new_size);
-
 	if (newPtr == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	content = newPtr;
-
 	while ((i < old_size) && (i < new_size))
 	{
-		content[i] = *ptrCopy++;
-		i++;
+		content[i++] = *ptrCopy++;
 	}
-
 	free(ptr);
 	return (newPtr);
 }
