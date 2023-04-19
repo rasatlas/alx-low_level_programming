03adb291e6214248c76b9b2f9d8b56f190cf5885
= <=>
2023-04-20 01:37:51 +0300

    Task 3 - Write a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,6 +6,31 @@
  * Return: integer value
  */
 
-int main(void)
+int main(int argc char argv[])
 {
+	if (argv[2] != '+' || argv[2] != '-' ||
+		argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+	{
+		prtintf("%s\n", "Error");
+		exit(99);
+	}
+
+	if (argc == 4)
+	{
+		int a, b;
+		int (*pfunc)(int int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		pfunc = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("%s\n", "Error");
+			exit(100);
+		}
+		printf("%d\n", pfunc(a, b));
+		return (0);
+	}
+	printf("%s\n", "Error");
+	exit(98);
 }
