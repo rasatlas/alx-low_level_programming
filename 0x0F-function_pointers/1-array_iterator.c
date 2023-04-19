5208a445c6ab8c0d3bcf33712a092a83e2f2e4da
= <=>
2023-04-19 21:10:27 +0300

    Task 1 - Write a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function that executes a function given as a parameter
+ * on each element of an array.
+ *
+ * @array: input array.
+ * @size: size of the input array.
+ * @action: input function to be executed on each element of array array.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (size > 0)
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
