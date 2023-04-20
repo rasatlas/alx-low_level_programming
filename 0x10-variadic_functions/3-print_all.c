99222e53c180304c78a98bf129761d3af80773f1
root <root@SUN>
2023-04-21 01:12:07 +0300

    Task 3 - Write a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that prints anything.
+ *
+ * @format: a list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+}
