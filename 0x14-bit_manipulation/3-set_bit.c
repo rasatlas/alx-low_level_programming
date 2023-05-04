c312fa63cc77ebf90128c47239501f651d31160c
Zemichael <yetikimitabeba@gmail.com>
2023-05-04 21:19:32 +0300

    Task 3.11 - Write a function that sets the value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * get_bit - Write a function that returns the value of a bit at a given index.
+ * @n: number to calculate the value of a bit at a given index upon.
+ * @index: is the index, starting from 0 of the bit you want to get.
+ * Return: the value of the bit at index index or -1 if an error occured.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	return (n >> index & 1);
+}
+
+/**
+ * set_bit - a function that sets the value of a bit to 1 at a given index.
+ * @n: value to set a bit to 1 at a given index upon.
+ * @index: is the index, starting from 0 of the bit you want to set.
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
+}
