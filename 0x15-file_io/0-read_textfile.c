f54399a63d9179965537b46381c8cfc4008bc8e3
Zemichael <yetikimitabeba@gmail.com>
2023-05-09 01:23:08 +0300

    Task 0 - betty edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,36 +11,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
-	char *buff;
+	ssize_t fd, size, wr;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
-		free(buff);
+		free(text);
 		return (0);
 	}
-	rd = read(fd, buff, letters);
-	if (r == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
+
+	size = read(fd, text, letters);
+	wr = write(STDOUT_FILENO, text, size);
+
 	close(fd);
-	w = write(STDOUT_FILENO, buff, r);
-	if (w == -1)
-	{
-		free(buff);
-		return (0);
-	}
-	if (w != r)
-		return (0);
-	return (r);
+
+	return (wr);
 }
