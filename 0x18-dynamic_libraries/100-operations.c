8a4bc1189ce3d3f40355cc32bb47372fdc6680be
Zemichael <yetikimitabeba@gmail.com>
2023-06-19 18:42:54 +0300

    Task 2 - Create a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - returns sum
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns difference
+ * @a: first operand
+ * @b: second operand
+ * Return: difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns product
+ * @a: first operand
+ * @b: second operand
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - returns integer division
+ * @a: first operand
+ * @b: second operand
+ * Return: int division of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns modulo
+ * @a: first operand
+ * @b: second operand
+ * Return: modulo of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
