a3aeae1745ab4b4f6b6fe46bcf76b3c539f8aa4c
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-12 13:55:44 +0300

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,5 +11,19 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	return (1);
+	size_t i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
