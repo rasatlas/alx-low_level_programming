055647fa42f61d528ea0bd27ba649dc4b41abade
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-12 15:17:16 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,26 +11,29 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t middle = floor(size / 2);
+	int left = 0;
+	int right = (int)(size - 1);
+	int middle;
 
 	if (!array)
 	{
 		return (-1);
 	}
 
-	while (middle < size && middle > 0)
+	while (left <= right)
 	{
+		middle = floor((left + right) / 2);
 		if (array[middle] == value)
 		{
 			return (middle);
 		}
 		else if (array[middle] < value)
 		{
-			middle = floor((middle + size) / 2);
+			left = middle + 1;
 		}
 		else
 		{
-			middle = floor(middle / 2);
+			right = middle - 1;
 		}
 	}
 	return (-1);
