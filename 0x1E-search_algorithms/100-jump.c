e0d71a535f5005ad418a4c14813f7d420a680c81
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-13 13:38:52 +0300

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,26 +1,6 @@
 #include "search_algos.h"
 
 /**
- * linearsearch - function to perform linear search
- * @left: left boundary
- * @right: right boundary
- * @array: pointer to the first element of array
- * @value: value to look for
- * Return: index of value or -1 if it doesn't match to any element
- */
-
-int linearsearch(int *array, int left, int right, int value)
-{
-	for (; left < right; left++)
-	{
-		if (array[left] == value)
-		{
-			return (left);
-		}
-	}
-	return (-1);
-}
-/**
 * jump_search - a function that searches for a value in a sorted
 * array of integers using jump search
 * @array: a pointer to the first element of the array to search in
@@ -32,28 +12,25 @@ int linearsearch(int *array, int left, int right, int value)
 
 int jump_search(int *array, size_t size, int value)
 {
+	int step = (int)floor(sqrt(size));
 	int left = 0;
-	int step = floor(sqrt((int)(size - 1)));
-	int right = step;
-	int isize = (int)size;
+	int right = left + step;
 
-	if (!array)
-	{
-		return (-1);
-	}
-	while (array[step] < value)
+	while (array[right] < value)
 	{
 		left = right;
 		right += step;
-		if ((right > (isize - 1)) && (left < (isize - 1)) && (array[left]) < value)
-		{
-			linearsearch(array, left, right, value);
-		}
-		else
+		if (left >= (int)(size - 1))
 		{
 			return (-1);
 		}
 	}
-	linearsearch(array, left, right, value);
+	for (; left < right; left++)
+	{
+		if (array[left] == value)
+		{
+			return (left);
+		}
+	}
 	return (-1);
 }
